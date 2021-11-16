627a8aa537f7845a5cc92eaf16448f5f34f815b6
Mist <59188378+Mist0090@users.noreply.github.com>
2021-11-16 23:04:43 +0900

    Add files via upload

---
+++ Clutt 4.5 peaceful/Clutt4.5/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Clutt4._5
+{
+    partial class warn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // warn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(5, 5);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "warn";
+            this.Opacity = 0.01D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "warn";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.warn_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Clutt 4.5 peaceful/Clutt4.5/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clutt4._5
+{
+    public partial class warn : Form
+    {
+        public warn()
+        {
+            InitializeComponent();
+            TransparencyKey = BackColor;
+        }
+        public static void Extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
+        {
+            //Important.DO NOT CHANGE!!!
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            using (Stream s = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName))
+            using (BinaryReader r = new BinaryReader(s))
+            using (FileStream fs = new FileStream(outDirectory + "\\" + resourceName, FileMode.OpenOrCreate))
+            using (BinaryWriter w = new BinaryWriter(fs))
+                w.Write(r.ReadBytes((int)s.Length));
+        }
+
+        private void warn_Load(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to run this program?" + Environment.NewLine + "This is malicious software!" + Environment.NewLine + "" + Environment.NewLine + "Click Yes to continue", "Clutt4.5", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                Environment.Exit(-1);
+            }
+            else
+            {
+                Last_Warning();
+            }
+        }
+
+        public void Last_Warning()
+        {
+            if (MessageBox.Show("THIS IS THE LAST WARNING!!!" + Environment.NewLine + "IF YOU RUN THE PROGRAM, YOUR COMPUTER WILL RECEIVE LARGE DAMAGE AND YOU WILL HAVE TO REINSTALL IT!" + Environment.NewLine + "" + Environment.NewLine + "DO YOU REALLY WANT TO RUN THIS DANGEROUS PROGRAM!?", "Clutt4.5", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                Environment.Exit(-1);
+            }
+            else
+            {
+                string TempPath = Path.GetTempPath();
+                Extract("Clutt4._5", TempPath, "Resources", "colorpcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "darkpcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "glitch2pcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "glitch3pcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "glitch4pcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "glitch5pcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "glitchpcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "lightpcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "rainbowpcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "scrasepcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "shakepcm.wav");
+                Extract("Clutt4._5", TempPath, "Resources", "tunelpcm.wav");
+                    run_clutt();
+            }
+        }
+        public void run_clutt()
+        {
+            this.Hide();
+            var NewForm = new Clutt4_5();
+            NewForm.ShowDialog();
+            this.Close();
+        }
+    }
+}
